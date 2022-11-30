c0bec74307b7aa8c5c4911017681083158de0516
Muhamed Zuberovic <muhamed.zuberovic@isatis.com>
2022-11-30 12:00:56 +0100

    Added the sweetalert and vuevalidate

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
+// Composition stuff in vue3
 import { createApp } from 'vue';
+// Router
 import router from "./router";
+// Validation
+import Vuelidate from 'vuelidate';
+
+// Our toaster library
+import sweetAlert from "./sweet-alert";
+
+// Mikey custom directives cause I kind of hate myself
+// import uiAuthGuard from '@/custom-directives/ui-auth-guard'
+
+
+
+// Main entry component
 import App from './App.vue';
 
-createApp(App)
-	.use(router)
-	.mount('#app')
+//createApp(App)
+//	.use(router)
+//	.use(Vuelidate)
+//	.mount('#app')
 
 
-//const app = createApp(App);
-//app.use(router);
-//app.mount('#app');
+const app = createApp(App);
+
+// Stuff that works with Vue out of the box
+app.use(router);
+app.use(Vuelidate);
+
+// Custom directives
+// app.directive('uiAuthGuard', uiAuthGuard);
+
+app.mount('#app');
